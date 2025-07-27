244980c1fe6213cbc4ce49ca8e23e611532a7622
Meiamonee <meiamonebr@hotmail.com>
2025-07-26 21:04:53 -0300

    Finalizando primeira parte do projeto , sujeito a melhoras futuras

--- Projeto/src/componente/Corpo/Corpo.jsx
+++ Projeto/src/componente/Corpo/Corpo.jsx
@@ -4,12 +4,16 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import '../Corpo/Corpo.css';
 
+import Perifericos from '../../assets/setup.webp'
+import Controle from '../../assets/controle.webp'
+import Teclado from '../../assets/teclado.webp'
+
 
 function App() {
     return (
         <div className='carrosel-container'>
             <div className="header-overlay">
-                <h1>POWER</h1>
+                <h1 className='logo-nome'>POWER</h1>
                 <div className='teste'>
                     <p>SOBRE</p>
                     <p>CONTATO</p>
@@ -18,7 +22,11 @@ function App() {
 
             <Swiper
                 modules={[Pagination, Autoplay]}
-                pagination={{ clickable: true }}
+                pagination={{ 
+                    clickable: true,
+                    bulletClass: 'swiper-pagination-bullet',
+                    bulletActiveClass: 'swiper-pagination-bullet-active'
+                }}
                 spaceBetween={20}
                 slidesPerView={1}
                 autoplay={{
@@ -28,25 +36,25 @@ function App() {
             >
                 <SwiperSlide>
                     <div className='carrosel-slide' style={{ background: '#ffb3ba' }}>
-                        <h3>Slide 1</h3>
+                        <img src={Perifericos} alt="periferico" />
                     </div>
                 </SwiperSlide>
 
                 <SwiperSlide>
                     <div className='carrosel-slide' style={{ background: '#bae1ff' }}>
-                        <h3>Slide 2</h3>
+                        <img src={Controle} alt="" />
                     </div>
                 </SwiperSlide>
 
                 <SwiperSlide>
                     <div className='carrosel-slide' style={{ background: '#baffc9' }}>
-                        <h3>Slide 3</h3>
+                        <img src={Teclado} alt="" />
                     </div>
                 </SwiperSlide>
             </Swiper>
             <div className='carrosel-overlay-bg'></div>
             <div className='overlay'>
-                <h2 className='title'>Descubra a revolução no mundo dos <br /> periféricos com a Power</h2>
+                <h2 className='title'>Descubra a revolução no mundo dos <br /> periféricos com a <strong className='power-azul'>Power</strong></h2>
                 <p className='descricao'>Aqui, tecnologia de ponta encontra inovação para transformar sua experiência digital. De teclados mecânicos precisos a mouses ergonômicos de alta performance, oferecemos produtos que elevam seu setup a um novo nível. Não se contente com o comum, escolha a excelência que só a Power proporciona. Explore agora e sinta a diferença!</p>
             </div>
         </div>
