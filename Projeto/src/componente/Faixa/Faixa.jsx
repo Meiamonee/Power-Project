b85599fa49a9a3fc6c873d24d7f9b3d327e51d2b
Meiamonee <meiamonebr@hotmail.com>
2025-07-27 14:36:16 -0300

    otimização para melhorar o site e deixar o codigo mais eficiente

--- Projeto/src/componente/Faixa/Faixa.jsx
+++ Projeto/src/componente/Faixa/Faixa.jsx
@@ -1,42 +1,24 @@
 import '../Faixa/Faixa.css';
 import Marquee from "react-fast-marquee";
+import faixaLogos from '../../assets/faixa.webp'; 
 
-// Importação corrigida (Corsair com logo preta)
-import logitech from '../../assets/logi.webp';
-import corsair from '../../assets/corsair.webp'; // <-- ATUALIZE AQUI
-import hyperx from '../../assets/hyperx.webp';
-import steel from '../../assets/Steelseries.webp';
-import red from '../../assets/redragon.webp';
-import cooler from '../../assets/cooler.svg';
-import razer from '../../assets/razer.webp';
-import rog from '../../assets/rog.webp';
-import aoc from '../../assets/aoc.webp';
-
-const marcas = [
-  { nome: 'Logitech', imagem: logitech },
-  { nome: 'Corsair', imagem: corsair },
-  { nome: 'HyperX', imagem: hyperx },
-  { nome: 'SteelSeries', imagem: steel },
-  { nome: 'Redragon', imagem: red },
-  { nome: 'Cooler Master', imagem: cooler },
-  { nome: 'Razer', imagem: razer },
-  { nome: 'ROG', imagem: rog },
-  { nome: 'AOC', imagem: aoc },
-  
-];
 
 function Faixa() {
+      const numberOfLogoStrips = 15; 
   return (
-    <div className="marquee-container">
-      <Marquee speed={50} gradient={false} pauseOnHover>
-        {marcas.map((marca) => (
-          <div className="marca" key={marca.nome}>
-            <img
-              src={marca.imagem}
-              alt={marca.nome}
-              loading="lazy"
-            />
-          </div>
+    <div className="sash-container">
+      <Marquee
+        speed={60}
+        gradient={false}
+        pauseOnHover={true}
+      >
+        {Array.from({ length: numberOfLogoStrips }).map((_, i) => (
+          <img
+            key={i}
+            src={faixaLogos}
+            alt={`Faixa de Logos Patrocinadores ${i + 1}`}
+            className="sash-image"
+          />
         ))}
       </Marquee>
     </div>
